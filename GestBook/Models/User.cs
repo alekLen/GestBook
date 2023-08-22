1bae5ced90434a9b09545ce9c2d9436fa44aea18
Elena <aleklenoch@gmail.com>
2023-08-22 18:59:01 +0300

    # атрибуты в классе User

--- GestBook/Models/User.cs
+++ GestBook/Models/User.cs
@@ -1,9 +1,15 @@
-﻿namespace GestBook.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestBook.Models
 {
     public class User
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Поле должно быть заполнено")]
+        [Display(Name = "Имя пользователя: ")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Поле должно быть заполнено")]
+        [Display(Name = "Пароль: ")]
         public string Password { get; set; }
         public ICollection<Message> Messages { get; set; }
     }
