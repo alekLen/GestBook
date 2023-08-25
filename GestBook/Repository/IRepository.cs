1d7b54c25864ff09798841408de6d4c3700a1fb7
Elena <aleklenoch@gmail.com>
2023-08-25 21:03:06 +0300

    # добавлен интерфейс для работы с БД
    необходимо абстрагировать контроллер от БД

---
+++ GestBook/Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using GestBook.Models;
+namespace GestBook.Repository
+{
+    public interface IRepository
+    {
+        Task<Salt> GetSalt(User user);      
+        Task<User> GetUser(string name);
+        Task AddUser(User user);
+        Task AddSalt(Salt s);
+        Task AddMessage(Message mess);
+        Task Save();
+    }
+} 
